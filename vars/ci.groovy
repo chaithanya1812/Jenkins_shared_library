a85baefe369b88a809e4e32103034bf92ff1b767
CHAITHANYA <111736742+chaithanya1812@users.noreply.github.com>
2023-11-05 23:32:00 +0530

    Update ci.groovy

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -15,6 +15,7 @@ def cal(int val){
     if (val<100) { 
          //If the condition is true print the following statement 
          println("The value is less than 100"); 
+         echo "hello chaitu value is less than 100"
       } else { 
          //If the condition is false print the following statement 
          println("The value is greater than 100"); 
