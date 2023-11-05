40deb835b55ba73c9706e0e249cf6106f4007d8e
CHAITHANYA <111736742+chaithanya1812@users.noreply.github.com>
2023-11-05 23:28:52 +0530

    Update ci.groovy

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -10,3 +10,14 @@ pipeline{
   }
 }
 }
+
+def cal(int val){
+    if (val<100) { 
+         //If the condition is true print the following statement 
+         println("The value is less than 100"); 
+      } else { 
+         //If the condition is false print the following statement 
+         println("The value is greater than 100"); 
+      }
+}
+
