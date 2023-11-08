68c6cbb3c37b4894531326d93b2341dd8f043b3c
CHAITHANYA <111736742+chaithanya1812@users.noreply.github.com>
2023-11-08 12:15:32 +0530

    Update ci.groovy

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,14 +1,31 @@
 def call(){
 pipeline{
-  agent any
-  stages{
-     stage('first'){
-       steps{
-         echo "hello chaitu-from shared library"
-       }
-     }
-  }
-}
+        agent any
+        stages{
+            stage('git clone'){
+                steps{
+                    echo "git cloneing"
+                }
+            }
+            stage('compile-build'){
+                steps{
+                    script{
+                        compile.language("${env.language}")
+                    }
+                }
+            }
+            stage('Quality-check'){
+                steps{
+                    echo "quality-checking"
+                }
+            }
+            stage('docker-build'){
+                steps{
+                    echo "docker build is on-gonig"
+                }
+            }
+        }
+    }
 }
 
 def cal(int val){
