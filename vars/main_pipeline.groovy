7f05339005f2b3d2e177e6c85fc04da9144a0c70
CHAITHANYA <111736742+chaithanya1812@users.noreply.github.com>
2023-11-08 11:53:55 +0530

    Create main_pipeline.groovy

---
+++ vars/main_pipeline.groovy
@@ -0,0 +1,32 @@
+def main_pipeline(){
+    pipeline{
+        agent any
+        stages{
+            stage('git clone'){
+                steps{
+                    echo "git cloneing"
+                }
+            }
+            stage('compile-build'){
+                steps{
+                    script{
+                        compile.language("${env.language}")
+                    }
+                }
+            }
+            stage('Quality-check'){
+                steps{
+                    echo "quality-checking"
+                }
+            }
+            stage('docker-build'){
+                steps{
+                    echo "docker build is on-gonig"
+                }
+            }
+        }
+    }
+}
+
+
+  
