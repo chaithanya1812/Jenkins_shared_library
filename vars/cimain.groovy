c2754671c8a60b59023816aa83f60a82d6de5e53
CHAITHANYA <111736742+chaithanya1812@users.noreply.github.com>
2024-08-12 10:34:07 +0530

    Update cimain.groovy

--- vars/cimain.groovy
+++ vars/cimain.groovy
@@ -25,39 +25,19 @@ def call(){
                     echo "docker build is on-gonig"
                 }
             }
+          stage('docker-build'){
+                steps{
+                    script {
+                        chaitu()
+                    }
+                }
+            }
         }
     }
 }
 
 def chaitu(){
-    pipeline{
-        agent any
-        stages{
-            stage('git clone'){
-                when{ expression { "${env.name}"=="chaitu" } }
-                steps{
-                    echo "git cloneing"
-                }
-            }
-            stage('compile-build'){
-                steps{
-                    script{
-                        compile.language("${env.language}")
-                    }
-                }
-            }
-            stage('Quality-check'){
-                steps{
-                    echo "quality-checking"
-                }
-            }
-            stage('docker-build'){
-                steps{
-                    echo "docker build is on-gonig"
-                }
-            }
-        }
-    }
+println "hello" 
 }
 
 
