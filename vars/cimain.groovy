c4b4840bb0100223fa2c8155192bfb261e279223
CHAITHANYA <111736742+chaithanya1812@users.noreply.github.com>
2024-08-12 10:31:17 +0530

    Update cimain.groovy

--- vars/cimain.groovy
+++ vars/cimain.groovy
@@ -29,6 +29,36 @@ def call(){
     }
 }
 
+def chaitu(){
+    pipeline{
+        agent any
+        stages{
+            stage('git clone'){
+                when{ expression { "${env.name}"=="chaitu" } }
+                steps{
+                    echo "git cloneing"
+                }
+            }
+            stage('compile-build'){
+                steps{
+                    script{
+                        compile.language("${env.language}")
+                    }
+                }
+            }
+            stage('Quality-check'){
+                steps{
+                    echo "quality-checking"
+                }
+            }
+            stage('docker-build'){
+                steps{
+                    echo "docker build is on-gonig"
+                }
+            }
+        }
+    }
+}
 
 
   
